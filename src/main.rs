a195ca4ed2f3d70d077a34b04632b6758212be42
ZYJ-q <2979049853@qq.com>
2023-07-03 03:37:40 +0800

    l

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ fn main(){
             equity_bian_map.insert(String::from("time"), Value::from(sr.get(0).unwrap())); 
             equity_bian_map.insert(String::from("name"), Value::from(7));
             equity_bian_map.insert(String::from("equity"), Value::from(sr.get(1).unwrap())); 
+            equity_bian_map.insert(String::from("type"), Value::from("futures"));
             equity_histories.push_back(Value::from(equity_bian_map));   
         }
     });
