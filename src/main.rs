16f02f822f95393fb92188b03e64778f1f4efc63
ZYJ-q <2979049853@qq.com>
2023-07-12 10:44:42 +0800

    p

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use csv::Reader;
 use net_worth::actors::trade_mapper;
 use serde_json::{Map, Value};
 use net_worth::actors::*;
-const CSV_PATH:&str = "./angus_equity_test3.csv";
+const CSV_PATH:&str = "./trader02_equity_test2.csv";
 
 fn main(){
     init();
@@ -18,7 +18,7 @@ fn main(){
             // println!("{}", sr.get(3).unwrap()); 
             equity_bian_map.insert(String::from("time"), Value::from(sr.get(0).unwrap())); 
             equity_bian_map.insert(String::from("name"), Value::from(3));
-            equity_bian_map.insert(String::from("equity"), Value::from(sr.get(1).unwrap())); 
+            equity_bian_map.insert(String::from("equity"), Value::from(sr.get(4).unwrap())); 
             equity_bian_map.insert(String::from("type"), Value::from("Futures"));
             equity_histories.push_back(Value::from(equity_bian_map));   
         }
