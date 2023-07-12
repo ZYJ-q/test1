8acc927ca45a8f0657c1d82946e2175a6f378d4d
ZYJ-q <2979049853@qq.com>
2023-07-12 16:35:41 +0800

    p

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use csv::Reader;
 use net_worth::actors::trade_mapper;
 use serde_json::{Map, Value};
 use net_worth::actors::*;
-const CSV_PATH:&str = "./btc2.csv";
+const CSV_PATH:&str = "./hmaker_equity_test1.csv";
 
 fn main(){
     init();
@@ -17,14 +17,14 @@ fn main(){
             let mut equity_bian_map: Map<String, Value> = Map::new();
             // println!("{}", sr.get(3).unwrap()); 
             equity_bian_map.insert(String::from("time"), Value::from(sr.get(0).unwrap())); 
-            equity_bian_map.insert(String::from("name"), Value::from(7));
-            equity_bian_map.insert(String::from("equity"), Value::from(sr.get(1).unwrap())); 
-            equity_bian_map.insert(String::from("type"), Value::from("futures"));
+            equity_bian_map.insert(String::from("name"), Value::from(13));
+            equity_bian_map.insert(String::from("equity"), Value::from(sr.get(2).unwrap())); 
+            // equity_bian_map.insert(String::from("type"), Value::from("futures"));
             equity_histories.push_back(Value::from(equity_bian_map));   
         }
     });
 
-    let res = trade_mapper::TradeMapper::insert_bian_equity(Vec::from(equity_histories.clone()));
+    let res = trade_mapper::TradeMapper::insert_equity_equity(Vec::from(equity_histories.clone()));
 
     println!("插入数据是否成功{}, {:?}",res,Vec::from(equity_histories.clone()));
 
