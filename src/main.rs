3fc5e430d9428801891be2c02cc519714ba11abc
ZYJ-q <2979049853@qq.com>
2023-08-09 15:43:52 +0800

    p

--- src/main.rs
+++ src/main.rs
@@ -14,20 +14,20 @@ fn main(){
     let data = trade_mapper::TradeMapper::get_equity().unwrap();
 
     // println!("获取到的权益数据{:?}", data );
-    let len = data.len();
+    let len = data.len() / 3;
 
     for i in 0..len{
         let mut equity_bian_map: Map<String, Value> = Map::new();
-        let name = data[i].name;
+        let name = data[i * 3].name;
         
         if name == 12{
-            let equity = &data[i].equity;
+            let equity = &data[i * 3].equity;
             let new_equity = &equity[1..equity.len()-1];
             // println!("处理之后权益{}", new_equity);
             let equitys:f64 = new_equity.parse().unwrap();
             let new_equitys = equitys - 4535.7;
             // println!("权益1111{}", new_equitys);
-        let time = &data[i].time;
+        let time = &data[i * 3].time;
         let new_time = &time[1..time.len()-1];
 
         // let new_times = format!("{}/{}/{} {}:{}:{}", ts[0], new_t_m, new_t_d, new_t_h, new_t_ms, new_t_s);
@@ -52,24 +52,24 @@ fn main(){
             
 
         } else {
-            let equity = &data[i].equity;
+            let equity = &data[i * 3].equity;
             // println!("权益{}", equity);
             let new_equity = &equity[1..equity.len()-1];
             // println!("处理之后权益{}", new_equity);
             let equitys:f64 = new_equity.parse().unwrap();
-        let time = &data[i].time;
+        let time = &data[i * 3].time;
         let new_time = &time[1..time.len()-1];
         println!("打印时间{}", new_time);
         // let t = NaiveDateTime::parse_from_str(&time[1..time.len()-1], "%Y/%m/%d %H:%M").unwrap();
-            let r#type = &data[i].r#type;
+            let r#type = &data[i * 3].r#type;
             let new_type = &r#type[1..r#type.len()-1];
             // let new_time = format!("{}", t);
             // println!("时间{}", new_time);
             let t = NaiveDateTime::parse_from_str(&new_time, "%-Y/%-m/%-d %-H:%-M").unwrap();
-        println!("时间1111111111{}", t);
+        // println!("时间1111111111{}", t);
         equity_bian_map.insert(String::from("name"), Value::from(name));
             equity_bian_map.insert(String::from("equity"), Value::from(equitys));
-            equity_bian_map.insert(String::from("time"), Value::from(new_time));
+            equity_bian_map.insert(String::from("time"), Value::from(t.to_string()));
             equity_bian_map.insert(String::from("type"), Value::from(new_type));
             equity_histories.push_back(Value::from(equity_bian_map));
             
