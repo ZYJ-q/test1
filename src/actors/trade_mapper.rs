3ad75c4e41cf68aa55347d8ba7bddf765640c71f
ZYJ-q <2979049853@qq.com>
2023-08-11 14:37:22 +0800

    p

--- src/actors/trade_mapper.rs
+++ src/actors/trade_mapper.rs
@@ -56,7 +56,7 @@ impl TradeMapper {
     // 连接数据库
     let mut conn = get_connect();
     let res = conn.query_map(
-      r"select * from total_bybit_equity where id >= 19336 and id <= 19590 and name = 11",
+      r"select * from total_bybit_equity where id >= 19337 and id <= 19591 and name = 13",
       |(id, name, time, equity)| {
         ByBitEquity{ id, name, time, equity}
       } 
